02c4c3951405790577d37c4cac7d5dd50719280d
Yuvaraj T <yuvaraj6168@gmail.com>
2025-02-03 10:46:05 +0530

    db connection str configure

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use("", productRouter);
 
 app.listen('5000', ()=> console.log('server running on 5000'))
 
-mongoose.connect('mongodb://localhost:27017/sample')
+mongoose.connect('mongodb+srv://Yuvaraj:181024yr@myprojectcluster.lbvcn.mongodb.net/?retryWrites=true&w=majority&appName=MyProjectCluster')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
