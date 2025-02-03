a950bad72372aae97ceb313b83f5affb60dcd23e
Yuvaraj118 <yuvaraj6168@gmail.com>
2025-02-03 10:28:38 +0530

    changes modified

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ app.listen('5000', ()=> console.log('server running on 5000'))
 mongoose.connect('mongodb://localhost:27017/sample')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
+
+
+    console.log("Successfully Updated");
+    
